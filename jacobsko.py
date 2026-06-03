644791e7666b969fbf0c6a395bc222f261c37155
jacobskoOSU <jacobsko@oregonstate.edu>
2026-06-03 12:42:56 -0700

    Delete jacobsko.py

--- jacobsko.py
+++
@@ -1,9 +0,0 @@
-#Contributor: Korbyn Jacobs
-#ONID: jacobsko
-#Term: Spring of 2026
-
-def introduce():
-    print("Hi, I'm Korbyn Jacobs, ONID jacobsko.")
-    
-    if __name__ == "__main__":
-        introduce()

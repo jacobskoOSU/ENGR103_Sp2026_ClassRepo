39fefacfa2d5bdba8a0edf5a2664361e654096de
jacobskoOSU <jacobsko@oregonstate.edu>
2026-06-03 12:43:11 -0700

    Add files via upload

---
+++ jacobsko.py
@@ -0,0 +1,9 @@
+#Contributor: Korbyn Jacobs
+#ONID: jacobsko
+#Term: Spring of 2026
+
+def introduce():
+    print("Hi, I'm Korbyn Jacobs, ONID jacobsko.")
+    
+if __name__ == "__main__":
+    introduce()
